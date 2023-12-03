607389204e6f3988cdd338b85169afbb07af87ab
Malith Madhushanka <malithm@Maliths-MacBook-Pro.local>
2023-12-03 12:45:41 +0530

    spring animation from scratch part2 - @animated completed

--- SpringAnimation/SpringAnimation/ContentView.swift
+++ SpringAnimation/SpringAnimation/ContentView.swift
@@ -10,13 +10,13 @@ import Observation
 
 struct CircleView: View {
     
-    var offset: Double
+    var offset: CGPoint
     
     var body: some View {
         ZStack {
             Circle()
                 .fill(.tertiary)
-                .frame(width: 100, height: 100)
+                .frame(width: 50, height: 50)
                 .offset(y: -200)
             
             Circle()
@@ -26,8 +26,8 @@ struct CircleView: View {
             
             Circle()
                 .stroke(.primary, lineWidth: 8)
-                .frame(width: 100, height: 100)
-                .offset(y: offset)
+                .frame(width: offset.x, height: offset.x)
+                .offset(y: offset.y)
         }
     }
 }
@@ -40,14 +40,14 @@ struct CircleView: View {
 // }
 
 @propertyWrapper
-struct Animated: DynamicProperty {
-    @State var springDouble: SpringDouble
+struct Animated<Value: SpringValueProtocol>: DynamicProperty {
+    @State var springDouble: SpringValue<Value>
     
-    init(wrappedValue: Double) {
-        self.springDouble = SpringDouble(value: wrappedValue)
+    init(wrappedValue: Value) {
+        self.springDouble = SpringValue(value: wrappedValue)
     }
     
-    var wrappedValue: Double {
+    var wrappedValue: Value {
         get {
             springDouble.value
         }
@@ -57,23 +57,80 @@ struct Animated: DynamicProperty {
         }
     }
     
-    var projectedValue: SpringDouble {
+    var projectedValue: SpringValue<Value> {
         springDouble
     }
 }
 
-@Observable
-class SpringDouble {
-    var value: Double
-    var target: Double
-    var velocity: Double = 0
+protocol SpringValueProtocol {
+    static func -(lhs: Self, rhs: Self) -> Self
     
-    init(value: Double) {
+    static func +(lhs: Self, rhs: Self) -> Self
+    
+    static func +=(lhs: inout Self, rhs: Self)
+    
+    func scaled(by scalar: Double) -> Self
+    
+    static var zero: Self { get }
+}
+
+extension Double: SpringValueProtocol {
+//    static func -(lhs: Double, rhs: Double) -> Double {
+//        lhs - rhs
+//    }
+//    
+//    static func +(lhs: Double, rhs: Double) -> Double {
+//        lhs + rhs
+//    }
+//    
+//    static func +=(lhs: inout Double, rhs: Double) {
+//        lhs += rhs
+//    }
+    
+    func scaled(by scalar: Double) -> Double {
+        self * scalar
+    }
+    
+    static public var zero: Double {
+        0
+    }
+}
+
+extension CGPoint: SpringValueProtocol {
+    static func + (lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+        CGPoint(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
+    }
+    
+    static func - (lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+        CGPoint(x: lhs.x - rhs.x, y: lhs.y - rhs.y)
+    }
+    
+    static func +=(lhs: inout CGPoint, rhs: CGPoint) {
+        lhs.x += rhs.x
+        lhs.y += rhs.y
+    }
+    
+    func scaled(by scalar: Double) -> CGPoint {
+        CGPoint(x: self.x * scalar, y: self.y * scalar)
+    }
+    
+    static public var zero: CGPoint {
+        CGPoint(x: 0, y: 0)
+    }
+}
+
+@Observable
+class SpringValue<Value: SpringValueProtocol> {
+    var value: Value
+    var target: Value
+    var velocity: Value = .zero
+    
+    init(value: Value) {
         self.value = value
         self.target = value
     }
     
-    func animate(to: Double) {
+    func animate(to: Value) {
         target = to
     }
     
@@ -84,33 +141,32 @@ class SpringDouble {
     func update(timeDelta: Double) {
         let displacement = value - target
         // HOOKS LAW
-        let springForce = -displacement * stiffness
-        let dampingForce = -velocity * damping
+        let springForce = displacement.scaled(by: -stiffness)
+        let dampingForce = velocity.scaled(by: -damping)
         let combinedForce = springForce + dampingForce
         // a = F/m
         let acceleration = combinedForce
-        velocity += acceleration * timeDelta
-        value += velocity * timeDelta
+        velocity += acceleration.scaled(by: timeDelta)
+        value += velocity.scaled(by: timeDelta)
     }
 }
 
 struct ContentView: View {
-    @State var offset: Double = 200
-    @Animated var offsetSpring = 200
+    @State var offset: CGPoint = CGPoint(x: 100, y: 200)
+    @Animated var offsetSpring: CGPoint = CGPoint(x: 100, y: 200)
     // @animated var offsetSpring = 200.0
     
     @State var lastDate: Date = Date()
     @State var timeDelta: Double = 0.0
+    
+    @State var isBig: Bool = true
     
     var body: some View {
         HStack(spacing: 40) {
             CircleView(offset: offset)
                 .animation(.spring, value: offset)
             
-            
             CircleView(offset: offsetSpring)
-            // CircleView(offset: offsetSpring)
-            
         }
         .background {
             TimelineView(.animation) {context in
@@ -124,9 +180,13 @@ struct ContentView: View {
             }
         }
         .onTapGesture {
-            offset *= -1
-//            offsetSpring.animate(to: -offsetSpring.target)
-             offsetSpring = offset
+            if isBig {
+                offset = CGPoint(x: 50, y: -200)
+            } else {
+                offset = CGPoint(x: 100, y: 200)
+            }
+            offsetSpring = offset
+            isBig.toggle()
         }
     }
 }
